332cab0e4d377cec7548d3ab94c910dce24e3a24
Louis Gesbert <louis.gesbert@ocamlpro.com>
2013-12-12 17:02:52 +0000

    Better build process now in two stages, so that we get a reusable opam base image, then add the packages on top of it

--- make.sh
+++ make.sh
@@ -1,15 +1,40 @@
 #!/bin/bash -ue
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <docker-image> <opam-packages>"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <docker-image> <entrypoint> <opam-packages>"
     exit 1
 fi
+
+BASE_IMAGE=DockerOpam/base
+
+# Build the base image
+if [ -z "$(docker images -q $BASE_IMAGE)" ]; then
+    docker build -t $BASE_IMAGE base
+fi
+
 DOCKER_IMAGE=$1; shift
-echo "$*" >packages
+ENTRYPOINT=$1; shift
 PACKAGES=("$@")
 
-git clone https://github.com/ocaml/opam-repository
-cd opam-repository
+mkdir -p build && cd build
+if [ ! -d opam-repository ]; then
+    git clone https://github.com/ocaml/opam-repository
+    cd opam-repository
+else
+    cd opam-repository
+    git pull
+    #git clean -fdx
+fi
 opam-admin make --resolve ${PACKAGES[@]}
 cd ..
-docker build -t "$DOCKER_IMAGE" .
+
+cat >Dockerfile <<EOF
+FROM $BASE_IMAGE
+ADD opam-repository /home/opam/opam-repository
+RUN opam init /home/opam/opam-repository
+RUN opam-installext ${PACKAGES[@]}
+CMD ["$ENTRYPOINT"]
+EOF
+
+cd ..
+docker build -t "$DOCKER_IMAGE" build
